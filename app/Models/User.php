cbb5c5c2e4ce7a58757336e103c4605877ba47a9
Wint Myat Phyo <winmyatphyo5@gmail.com>
2026-06-26 10:58:51 +0630

    feat: implement database layer for RBAC system (roles, permissions, pivot relationships)

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,6 +23,11 @@ class User extends Authenticatable
         'password',
     ];
 
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class, 'role_user');
+    }
+
     /**
      * The attributes that should be hidden for serialization.
      *
